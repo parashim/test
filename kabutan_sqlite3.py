5f9d520792803eec8fa9c9828b9b964b34a50a86
parashim <haranameg@outlook.jp>
2020-06-24 22:37:36 +0900

    kabutan sqlite finalversion

---
+++ kabutan_sqlite3.py
@@ -0,0 +1,51 @@
+import time
+import requests
+import lxml.html
+import sqlite3
+
+def main():
+    for i in range(1300,1400):
+        brand = get_brand(i)
+        time.sleep(2)
+
+        if brand:
+            insert_brands_to_db(brand)
+        else:
+            print(i)
+
+def get_brand(code):
+    url = 'https://kabutan.jp/stock/?code={}'.format(code)
+    #code={} .format のふるまい
+
+    response = requests.get(url)
+    html = lxml.html.fromstring(response.text)
+
+    if len(html.cssselect('#kobetsu_right > div.company_block > h3')) == 0:
+        return None
+    try:
+        name = html.cssselect('#kobetsu_right > div.company_block > h3')[0].text
+        short_name =  html.cssselect('#kobetsu_right > div.company_block > h3')[0].text
+        market = html.cssselect('#stockinfo_i1 > div.si_i1_1 > span')[0].text
+        unit_str = html.cssselect('#kobetsu_left > table:nth-child(4) > tbody > tr:nth-child(6) > td')[0].text
+        unit = int(unit_str.split()[0].replace(',', ''))
+        sector = html.cssselect('#stockinfo_i2 > div > a')[0].text
+    except ValueError:
+        return None
+
+    return code,name,short_name,market,unit,sector
+
+def insert_brands_to_db(brand):
+    conn = sqlite3.connect("kabutan.db")
+#    print(brand)
+
+    with conn:
+        sql = 'INSERT INTO brands(code,name,short_name,market,unit,sector) ' \
+              'VALUES(?,?,?,?,?,?)'
+
+        try:
+            conn.execute(sql,brand)
+        except sqlite3.Error as e:
+            print(brand)
+
+if __name__=='__main__':
+    main()
